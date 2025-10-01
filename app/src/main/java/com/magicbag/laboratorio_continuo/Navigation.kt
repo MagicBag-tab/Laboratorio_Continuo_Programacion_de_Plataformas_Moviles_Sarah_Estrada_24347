2eb83c5ca8b3419bd7ddd1484c553bd5f7914199
Sarah <est24347@uvg.edu.gt>
2025-09-30 20:13:42 -0600

    Laboratorio 10

--- app/src/main/java/com/magicbag/laboratorio_continuo/Navigation.kt
+++ app/src/main/java/com/magicbag/laboratorio_continuo/Navigation.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
